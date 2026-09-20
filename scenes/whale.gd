6e6918f50a230492839ada2524d52818a0c4a303
s0lst1ce <cadmium-beaver@pm.me>
2026-09-20 15:27:24 +0200

    feat: whale meeting

--- scenes/whale.gd
+++ scenes/whale.gd
@@ -2,4 +2,7 @@ extends Interactible
 
 
 func _on_click() -> void:
-	Dialogic.start("arrival")
+	print(Dialogic.VAR.NextDialogue.whale)
+	if Dialogic.VAR.NextDialogue.whale != null:
+		Dialogic.start(Dialogic.VAR.NextDialogue.whale)
+	#Dialogic.VAR.NextDialogue.whale = null
