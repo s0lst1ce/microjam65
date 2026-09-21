a0372bee008bbc912362234d7ec1095f960f159b
s0lst1ce <cadmium-beaver@pm.me>
2026-09-21 11:46:12 +0200

    fix: inventory assets

--- scene_switching.gd
+++ scene_switching.gd
@@ -4,6 +4,4 @@ extends Node
 signal goto_room(room: String)
 signal toggle_map()
 signal toggle_hud_visibility()
-signal disable_inventory()
-signal enable_inventory()
 signal toggle_inventory_visibility()
