0668f21ede810c066f5fc556368164c74ae2a405
s0lst1ce <cadmium-beaver@pm.me>
2026-09-21 21:11:54 +0200

    fix: another attempt at perf issue

--- interactibles/painting_swordfish.gd
+++ interactibles/painting_swordfish.gd
@@ -1,7 +1,16 @@
 extends Interactible
 
+@onready var filled = preload("res://assets/furniture/painting_completed.png")
+
+func _ready() -> void:
+	super ._ready()
+	Enigma.drop_diamond.connect(_complete_painting)
+
 func _on_click() -> void:
 	if Dialogic.VAR.puzzle_pieces_count == 36:
 		$PaintingPuzzle.show()
 	else:
 		Dialogic.start("puzzle_not_enough_pieces")
+
+func _complete_painting():
+	texture = filled
